bccbf608caa6a38ac65b061f90a4705e95101141
Jorge Moreno <solisamo6@gmail.com>
2019-09-06 21:43:00 -0500

    Se añadieron las clases funcionales y estructuradas

--- lib/gameoflife.rb
+++ lib/gameoflife.rb
@@ -1,7 +1,12 @@
 require "./gameoflife/version"
+require './gameoflife/cell'
+require './gameoflife/game_board'
+require './gameoflife/game'
 
-puts 'Game of Life'
+puts "\nGame of Life\n\n"
 print 'Choose the size of the board: '
 size = gets.chomp.to_i
 print 'Choose the generations you want: '
 generations = gets.chomp.to_i
+
+GameofLife::Game.new(size, generations)
