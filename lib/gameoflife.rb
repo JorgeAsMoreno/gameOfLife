7cb0f52afeb16f9e33922c0757ebea67bba80280
Jorge Moreno <solisamo6@gmail.com>
2019-09-06 21:47:19 -0500

    Last commit

--- lib/gameoflife.rb
+++ lib/gameoflife.rb
@@ -4,8 +4,10 @@ require './gameoflife/game_board'
 require './gameoflife/game'
 
 puts "\nGame of Life\n\n"
+
 print 'Choose the size of the board: '
 size = gets.chomp.to_i
+
 print 'Choose the generations you want: '
 generations = gets.chomp.to_i
 
